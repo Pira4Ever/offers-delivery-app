ca1ae402c49277f29379e4b942805aac207919fd
Octavio Piratininga <59701790+Pira4Ever@users.noreply.github.com>
2026-09-15 10:30:22 -0300

    fix: roldao due date scrapping

--- OffersDelivery.Core/ApiClient.cs
+++ OffersDelivery.Core/ApiClient.cs
@@ -86,16 +86,19 @@ public partial class ApiClient(HttpClient httpClient, OfferRepository repository
                         }));
 
                     var jsonResp = await response.Content.ReadFromJsonAsync<RoldaoApiResponse>();
-                    string dateString;
+                    DateTime dueDate;
                     try
                     {
-                        dateString = item.QuerySelector(".periodo-oferta")!.TextContent.Split(" a ")[1];
+                        dueDate = DateTime.ParseExact(item.QuerySelector(".periodo-oferta")!.TextContent.Split(" a ")[1], "dd.MM", CultureInfo.InvariantCulture);
                     }
                     catch (IndexOutOfRangeException)
                     {
-                        dateString = item.QuerySelector(".periodo-oferta")!.TextContent;
+                        dueDate = DateTime.ParseExact(item.QuerySelector(".periodo-oferta")!.TextContent, "dd.MM", CultureInfo.InvariantCulture);
                     }
-                    var dueDate = DateTime.ParseExact(dateString, "dd.MM", CultureInfo.InvariantCulture);
+                    catch (Exception)
+                    {
+                        dueDate = DateTime.Now.AddDays(5);
+                    }
                     dueDate = dueDate < DateTime.Today ? dueDate.AddYears(1) : dueDate;
 
                     offers.Add(new GetOffersResponseDto
